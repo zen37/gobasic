92359e54ab8da3f171933d95eff31eca7c8f59e2
zen <zen@zen>
2021-08-07 17:08:32 -0700

    read nested yaml file

---
+++ yaml/go.mod
@@ -0,0 +1,5 @@
+module github.com/zen37/gobasic/yaml
+
+go 1.16
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ yaml/read_nested_yaml.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type yamlConfig struct {
+	Servers []struct {
+		Server    string `yaml:"server"`
+		Name      string `yaml:"name"`
+		Databases []struct {
+			Database string `yaml:"database"`
+			Name     string `yaml:"name"`
+			Tables   []struct {
+				Table string `yaml:"table"`
+				Name  string `yaml:"name"`
+			} `yaml:"tables"`
+		} `yaml:"databases"`
+	} `yaml:"servers"`
+}
+
+var cfg yamlConfig
+
+func init() {
+
+	yamlConfig, err := ioutil.ReadFile(".config.yaml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = yaml.Unmarshal(yamlConfig, &cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(cfg)
+}
+
+func main() {
+
+}
