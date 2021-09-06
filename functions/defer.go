eb7da2ef309d5e3cbcc90b320d20b3d92bcb029f
zen <zen@zen>
2021-09-06 09:40:11 -0700

    Return all of the errors as []error

--- functions/defer.go
+++ functions/defer.go
@@ -1,33 +1,49 @@
+//https://trstringer.com/golang-deferred-function-error-handling/
+
 package main
 
 import "fmt"
 
 func cleanup() error {
-	fmt.Println("Running cleanup...")
-	return nil
+	_, err := fmt.Println("Running cleanup...")
+	return err
+	//return fmt.Errorf("error from cleanup")
 }
 
 func doThis() error {
 	return fmt.Errorf("error from doing another thing")
 }
 
-func getMessage() (msg string, err error) {
+func getMessage() (msg string, errs []error) {
 
 	defer func() {
-		if errDefferred := cleanup(); errDefferred != nil {
-			err = errDefferred
+		if err := cleanup(); err != nil {
+			errs = append(errs, err)
 		}
 
 	}()
-	err = doThis()
+	if err := doThis(); err != nil {
+		errs = append(errs, err)
+	}
 
-	return "hello world", err
+	return "hello world", errs
 }
 
 func main() {
-	message, err := getMessage()
-	if err != nil {
-		fmt.Printf("Error getting message: %v\n", err)
+	/*
+		message, err := getMessage()
+		if err != nil {
+			fmt.Printf("Error getting message: %v\n", err)
+		} else {
+			fmt.Printf("Success. Message: '%s'\n", message)
+		}
+	*/
+	message, errs := getMessage()
+	if errs != nil {
+		fmt.Printf("There are %d error(s)\n", len(errs))
+		for _, err := range errs {
+			fmt.Printf("Error: %v\n", err)
+		}
 	} else {
 		fmt.Printf("Success. Message: '%s'\n", message)
 	}
