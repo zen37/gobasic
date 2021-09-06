fdc7dad6c5eec73ff2df0d9f90bfe70a071c8abc
zen <zen@zen>
2021-09-06 08:33:31 -0700

    init

---
+++ functions/defer.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func cleanup() error {
+	fmt.Println("Running cleanup...")
+	return fmt.Errorf("error on cleanup")
+}
+
+func getMessage() (string, error) {
+
+	cleanup()
+
+	return "hello world", nil
+}
+
+func main() {
+	message, err := getMessage()
+	if err != nil {
+		fmt.Printf("Error getting message: %v\n", err)
+	} else {
+		fmt.Printf("Success. Message: '%s'\n", message)
+	}
+}
