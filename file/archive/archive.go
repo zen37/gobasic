77cac103be901392091abdd9d0f6bf4415d0c74e
zen <zen@zen>
2022-02-05 19:50:02 -0800

    read archive files

---
+++ file/archive/archive.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+)
+
+func main() {
+
+	zrc, err := zip.OpenReader("fs.zip")
+	//zrc, err := zip.OpenReader("readme.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer zrc.Close()
+
+	for _, file := range zrc.File {
+		fmt.Printf("file %s:\n", file.Name)
+
+		rc, err := file.Open()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer rc.Close()
+
+		content, err := readFile(rc)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		displayFile(content)
+
+	}
+}
+
+func readFile(rc io.ReadCloser) ([]byte, error) {
+
+	b, err := io.ReadAll(rc)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+
+}
+
+func displayFile(b []byte) {
+	fmt.Printf(("%s\n\n"), b)
+	//fmt.Println(string(content))
+}
