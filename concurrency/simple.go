6a794f12f4f3470ac3c924070d158ec131885fdf
zen <zen@zen>
2022-02-27 18:36:52 -0800

    concurency with channels

--- concurrency/simple.go
+++ concurrency/simple.go
@@ -5,22 +5,45 @@ import (
 	"time"
 )
 
+var doneNumbers chan struct{}
+var doneLetters chan struct{}
+
 func main() {
+
+	doneNumbers = make(chan struct{})
+	doneLetters = make(chan struct{})
+
 	go print_letters()
 	go print_numbers()
-	time.Sleep(5000 * time.Millisecond)
+	//	time.Sleep(5000 * time.Millisecond)
+
+	<-doneLetters
+	fmt.Println()
+	fmt.Println("print_letters() finished")
+
+	<-doneNumbers
+	fmt.Println()
+	fmt.Println("print_numbers() finished")
 }
 
 func print_letters() {
+
+	fmt.Println("print_letters started")
+
 	for i := 'a'; i <= 'z'; i++ {
 		fmt.Printf(" %c", i)
 		time.Sleep(100 * time.Millisecond)
 	}
+	doneLetters <- struct{}{}
 }
 
 func print_numbers() {
-	for i := 0; i <= 27; i++ {
+
+	fmt.Println("print_numbers started")
+
+	for i := 0; i <= 33; i++ {
 		fmt.Print(" ", i)
 		time.Sleep(100 * time.Millisecond)
 	}
+	doneNumbers <- struct{}{}
 }
